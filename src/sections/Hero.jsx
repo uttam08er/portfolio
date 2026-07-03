74eb9d3855d23d5725ad0983d22cc11f08479c9a
Uttam Kumar <uttamkssm2000.com>
2026-07-04 00:41:09 +0530

    Commit done

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -387,7 +387,7 @@ Best regards,
                     height: '380px',
                     borderRadius: '50%'
                   }}>
-                  <img src="./public/hero-imag.jpg" alt="hero-image"
+                  <img src="/hero-image.jpg" alt="hero-image"
                     style={{
                       height: '380px',
                       borderRadius: '50%',
